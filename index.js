56e3748fffc4e3fcf2b2e742af85e3e3a88571e5
Mike Andrade <hoodloom07@outlook.es>
2023-02-12 20:50:08 -0600

    Cicle with elemnts about arrow for the eventListener each skill's section

--- index.js
+++ index.js
@@ -37,16 +37,17 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // SKILLS SECTION ANIMATION
 
-const skillsContent = document.getElementById('skills__content'),
-        skillsHeader = document.querySelectorAll('.skills__header');
-
+const skillsContent = document.getElementsByClassName('skills__content'),
+        skillsHeader = document.querySelectorAll('.skills__header'); /* Elements in all nodeList*/
 
 function toggleSkills(){
-    let itemClass = this.parentNode.getElementByClassName
+    let itemClass = this.parentNode.className
 
-    for(i = 0; i < skillsContent.clientHeight; i++){
+    for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
-    } if(itemClass === 'skills__content skills__close'){
+    } if(itemClass === 'skills__content skills__close'){ // Change class close to class open
         this.parentNode.className = 'skills__content skills__open'
     }
 }
+
+skillsHeader.forEach((el =>{el.addEventListener('click', toggleSkills)}))
